8139e60ad06c802a22d6e87eb3e0bd95195f0af1
Wesley <wes.beck21@outlook.com>
2021-03-01 18:15:26 -0800

    commit tree 36d3ed9755b2fef5fb73e33e2ea13cbc6a6df37a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(arr, cb) {
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(cb(arr[i]))
+  }
+  return newArr
+}
+
+function reduce(arr, cb, starting) {
+  let r = (!!starting) ? starting : arr[0]
+  let i = (!!starting) ? 0 : 1
+  
+  for ( ; i < arr.length; i++) {
+    r = cb(arr[i], r)
+  }
+  return r;
+}
